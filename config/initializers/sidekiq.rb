37176e7d85877d36171efe3f9633e5db5f867883
Katsuma Narisawa <katsuma.narisawa@gmail.com>
2021-01-11 15:14:24 +0900

    Remove unnecessary redis settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,11 +5,3 @@ require 'sidekiq/web'
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == ['admin', ENV['BASIC_AUTH_PASSWORD'] || 'password']
 end
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_SERVER_HOSTNAME']}:#{ENV['REDIS_SERVER_PORT']}/0" }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_SERVER_HOSTNAME']}:#{ENV['REDIS_SERVER_PORT']}/0" }
-end
